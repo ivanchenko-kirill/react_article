1684efd6f635c5118d615647453fc71ec441e01d
Kirill Ivanchenko <ivanchenkok557@gmail.com>
2021-08-22 21:34:31 +0300

    Updates

---
+++ precache-manifest.82c65a84b13e18f5469b604c84f19b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2fea21a852252de0f51089c8aad7f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb09ef13d805c5869d5b",
+    "url": "./static/js/main.66f0e23c.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.82c65a84b13e18f5469b604c84f19b83.js"
 );
 
 self.addEventListener('message', (event) => {
